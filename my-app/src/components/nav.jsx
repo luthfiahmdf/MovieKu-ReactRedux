46807694d33159b87da4e162a3a05042571e348f
luthfiahmdf <luthfiahmd@gmail.com>
2022-11-16 19:11:30 +0700

    navbar

--- my-app/src/components/nav.jsx
+++ my-app/src/components/nav.jsx
@@ -1,71 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import { Navbar } from "flowbite-react";
 import { Avatar } from "flowbite-react";
 import { Dropdown } from "flowbite-react";
 import logo from "./assets/movieku.png";
+import { BiSearchAlt } from "react-icons/bi";
+import { FaBars } from "react-icons/fa";
 import "./components.css";
 import { useNavigate } from "react-router-dom";
 function Nav() {
+  const [navbarOpen, setNavbarOpen] = useState(false);
   const navigate = useNavigate();
   return (
-    <div className="container bg-primary-100">
-      <Navbar fluid={true} rounded={true} className="mx-9">
-        <Navbar.Brand href="/">
-          <img src={logo} className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
-          {/* <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white text-white">
-            Flowbite
-          </span> */}
-        </Navbar.Brand>
-        <div className="flex md:order-2">
-          <Dropdown
-            arrowIcon={false}
-            inline={true}
-            label={
-              <Avatar
-                alt="User settings"
-                // img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-                rounded={true}
-              />
-            }
-          >
-            <Dropdown.Header>
-              <span className="block text-sm">Bonnie Green</span>
-              <span className="block truncate text-sm font-medium">
-                name@flowbite.com
-              </span>
-            </Dropdown.Header>
-            <Dropdown.Item>Dashboard</Dropdown.Item>
-            <Dropdown.Item>Settings</Dropdown.Item>
-            <Dropdown.Item>Earnings</Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Item>Sign out</Dropdown.Item>
-          </Dropdown>
-          <Navbar.Toggle />
-        </div>
-        <Navbar.Collapse className="nav">
-          <Navbar.Link
-            href="/"
-            style={{
-              color: "white",
-            }}
-            className="hover:text-yellow-300"
-          >
-            Home
-          </Navbar.Link>
-          <Navbar.Link
-            href="/search"
-            style={{
-              color: "white",
-            }}
-            className="hover:text-yellow-300 text-yellow-300"
-          >
-            Search
-          </Navbar.Link>
-
-          {/* <Navbar.Link href="/navbars">Pricing</Navbar.Link>
-          <Navbar.Link href="/navbars">Contact</Navbar.Link> */}
-        </Navbar.Collapse>
-      </Navbar>
+    <div className="container bg-transparent nav">
+      <>
+        <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-pink-500 mb-3">
+          <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+            <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+              <a
+                className="text-2xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+                href="#pablo"
+              >
+                Movie <span className="text-yellow-300">Ku</span>
+              </a>
+              <button
+                className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+                type="button"
+                onClick={() => setNavbarOpen(!navbarOpen)}
+              >
+                <i className="fas fa-bars">
+                  <FaBars />
+                </i>
+              </button>
+            </div>
+            <div
+              className={
+                "lg:flex flex-grow items-center" +
+                (navbarOpen ? " flex" : " hidden")
+              }
+              id="example-navbar-danger"
+            >
+              <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+                <li className="nav-item">
+                  <a
+                    className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                    href="#pablo"
+                  >
+                    <i className="fab fa-facebook-square text-lg leading-lg text-white opacity-75"></i>
+                    <span className="ml-2">Share</span>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a
+                    className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                    href="#pablo"
+                  >
+                    <i className="fab fa-twitter text-lg leading-lg text-white opacity-75"></i>
+                    <span className="ml-2">Tweet</span>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a
+                    className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                    href="#pablo"
+                  >
+                    <i className="fab fa-pinterest text-lg leading-lg text-white opacity-75"></i>
+                    <span className="ml-2">Pin</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </>
     </div>
   );
 }
